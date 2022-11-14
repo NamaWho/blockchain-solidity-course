cb9ba595a2a092b14767db158243d2f3183f9ce7
Daniel Namaki <namaki.work@gmail.com>
2022-11-14 15:06:59 +0100

    SafeMath, Overflow Check, "unchecked" [fund_me]

---
+++ fund_me/SafeMathTester.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+// This old version is because nowadays it wouldn't compile because of the reasons explained in the contract
+// pragma solidity ^0.6.0;
+// We can using newer versions if us the "unchecked" keyword
+pragma solidity ^0.8.0;
+
+
+
+// SafeMath library tester
+// No longer used due to bug reveiled
+contract SafeMathTester {
+
+    // this variable is "unchecked", which means that if it gets over its upper bound, it wraps around and restarts from it lower bound
+    uint8 public bigNumber = 255;
+    
+    // this function would set bigNumber to 0, due to variable size (8 bit)
+    function add() public {
+        unchecked {
+          bigNumber = bigNumber + 1;   
+        }
+    }
+
+}
