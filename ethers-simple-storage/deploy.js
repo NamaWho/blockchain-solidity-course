e4b29274f74e5acee3cc85b26a85a22d55c433cf
Daniel Namaki <namaki.work@gmail.com>
2022-11-21 20:42:51 +0100

    Deploy contract using ethers+fs [ethersjs_storage]

--- ethers-simple-storage/deploy.js
+++ ethers-simple-storage/deploy.js
@@ -16,10 +16,40 @@
 // > Fulfilled
 // > Rejected
 
+const ethers = require("ethers");
+const fs = require("fs");
+
 async function main() {
-  console.log("hello");
-  let variable = 5;
-  console.log(variable);
+  // Compilation command via solcjs [yarn]
+  // yarn solcjs --bin --abi --include-path node_modules --base-path . -o . SimpleStorage.sol
+  // Compiling each time in this way could be annoying, so we add a script to our package.json
+
+  // In order to connect to our local Ganache blockchain we connect to the RPC endpoint at http://127.0.0.1:7545
+  // ethers is going to connect to the RPC endpoint automatically behind the scenes
+  // This script is now going to connect with blockchain via the given enpoint url (blockchain node)
+  const provider = new ethers.providers.JsonRpcProvider(
+    "http://127.0.0.1:7545"
+  );
+
+  // Not a good practice, but useful to learn
+  const wallet = new ethers.Wallet(
+    "ad5d78cd52c03e967dad0be010458f40581d3c0818e110307aeff2908dfdf649",
+    provider
+  );
+
+  const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
+  const binary = fs.readFileSync(
+    "./SimpleStorage_sol_SimpleStorage.bin",
+    "utf8"
+  );
+
+  // A Contract Factory is used by ehters to deploy contract
+  const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
+
+  console.log("Deploying , please wait...");
+  const contract = await contractFactory.deploy(); // STOP here! Wait for contract to deploy
+
+  console.log(contract);
 }
 
 main()
